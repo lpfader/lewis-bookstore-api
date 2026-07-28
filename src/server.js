02925b83f75915557c86e3c4d331f4c60919eefb
lpfader <lpfader@gmail.com>
2026-07-28 13:28:32 -0400

    edited serverjs for port issues

--- src/server.js
+++ src/server.js
@@ -234,4 +234,5 @@ users.count({}, (err, count) => {
 });
 
 
-app.listen(3000, () => console.log("Lewis' Bookstore API is running on http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Lewis' Bookstore API is running on port ${PORT}`));
